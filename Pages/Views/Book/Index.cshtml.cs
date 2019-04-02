5a87715db668063d42b16e73601421ebad2d78dc
Colburn Sanders <colburnsanders@gmail.com>
2019-04-02 08:28:28 -0500

    updated index page model with dependency injection

--- Pages/Views/Book/Index.cshtml.cs
+++ Pages/Views/Book/Index.cshtml.cs
@@ -2,16 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListRazor.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookListRazor.Pages.Views.Book
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly BookDbContext _db;
 
+        public IndexModel(BookDbContext db)
+        {
+            _db = db;
+        }
+
+        public List<Data.Book> Books { get; set; }
+
+        public async Task OnGet()
+        {
+            Books = await _db.Books.ToListAsync();
         }
     }
 }
